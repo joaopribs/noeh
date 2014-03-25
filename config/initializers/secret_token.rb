5b77f76d0081aa076f8591f51f070cb0f2237983
Joao Paulo Ribeiro <joaopaulo@quimbik.com>
2014-03-25 17:07:33 -0300

    Import inicial do código criado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SistemaIgreja::Application.config.secret_key_base = '18ad3581e3944d692a0001203c7d67c869a72cc0d6533ac5e4ba98aada9424e4b6675fe75a9bf620f4d30e1dc756d23099705efc350215550462f157d52d7fec'
