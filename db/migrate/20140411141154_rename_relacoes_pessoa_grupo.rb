5658a27b5c69b08c1855b531c459900c02d0ecff
Joao Paulo Ribeiro <joaopaulo@quimbik.com>
2014-05-03 11:13:35 -0300

    Tentando resolver problema de migracao inicial

--- db/migrate/20140411141154_rename_relacoes_pessoa_grupo.rb
+++ db/migrate/20140411141154_rename_relacoes_pessoa_grupo.rb
@@ -1,5 +1,5 @@
 class RenameRelacoesPessoaGrupo < ActiveRecord::Migration
   def change
-    rename_table :relacoes_pessoa_grupo, :relacoes_membro_grupo
+    # rename_table :relacoes_pessoa_grupo, :relacoes_membro_grupo
   end
 end
