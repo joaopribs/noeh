30ce198dce336061a02e181aef4418643615c8ae
Joao Paulo Ribeiro <joaopaulo@quimbik.com>
2014-06-01 11:22:27 -0300

    Tentativa 3 de rake task pra resetar o db

--- db/migrate/20140526230623_remover_campos_de_forma_de_encontros.rb
+++ db/migrate/20140526230623_remover_campos_de_forma_de_encontros.rb
@@ -1,9 +1,9 @@
 class RemoverCamposDeFormaDeEncontros < ActiveRecord::Migration
   def change
-    remove_column :forma_de_encontros, :numeroSolteirosCoordEquipe
-    remove_column :forma_de_encontros, :numeroCasaisCoordEquipe
-    remove_column :forma_de_encontros, :numeroSolteirosCoordPermanente
-    remove_column :forma_de_encontros, :numeroCasaisCoordPermanente
-    rename_column :forma_de_encontros, :nomeConjuntoPermanente, :nome_conjunto_permanente
+    # remove_column :forma_de_encontros, :numeroSolteirosCoordEquipe
+    # remove_column :forma_de_encontros, :numeroCasaisCoordEquipe
+    # remove_column :forma_de_encontros, :numeroSolteirosCoordPermanente
+    # remove_column :forma_de_encontros, :numeroCasaisCoordPermanente
+    # rename_column :forma_de_encontros, :nomeConjuntoPermanente, :nome_conjunto_permanente
   end
 end
