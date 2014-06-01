4a91b24625530e7a81a23e48b96dc811b9f43ab4
Joao Paulo Ribeiro <joaopaulo@quimbik.com>
2014-06-01 11:26:18 -0300

    Consertando os migrations

--- db/migrate/20140526230623_remover_campos_de_forma_de_encontros.rb
+++
@@ -1,9 +0,0 @@
-class RemoverCamposDeFormaDeEncontros < ActiveRecord::Migration
-  def change
-    # remove_column :forma_de_encontros, :numeroSolteirosCoordEquipe
-    # remove_column :forma_de_encontros, :numeroCasaisCoordEquipe
-    # remove_column :forma_de_encontros, :numeroSolteirosCoordPermanente
-    # remove_column :forma_de_encontros, :numeroCasaisCoordPermanente
-    # rename_column :forma_de_encontros, :nomeConjuntoPermanente, :nome_conjunto_permanente
-  end
-end
--- db/migrate/20140526232337_remover_forma_de_encontros.rb
+++
@@ -1,5 +0,0 @@
-class RemoverFormaDeEncontros < ActiveRecord::Migration
-  def change
-    # drop_table :forma_de_encontros
-  end
-end
