6b5d5294e3c938bb0ed411f7317a7cdddfeb8523
Joao Paulo Ribeiro <joaopaulo@quimbik.com>
2014-06-07 20:01:06 -0300

    Skip notificacao no log_out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@
 class SessionsController < ApplicationController
   skip_before_filter :precisa_estar_logado, :only => [:log_in, :log_out]
   skip_before_filter :verify_authenticity_token
+  skip_before_filter :notificacao
 
   def log_in
 
