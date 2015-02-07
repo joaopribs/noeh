7e6ebd8b1ebeb9aad8dcb3f706ad6203c755e05f
Joao Paulo Ribeiro da Silva <joaop.ribs@gmail.com>
2015-02-07 09:31:24 -0600

    Tentando resolver problema com migração

--- db/migrate/20150125005230_add_equipe_padrao_relacionada_to_conjunto_pessoas.rb
+++ db/migrate/20150125005230_add_equipe_padrao_relacionada_to_conjunto_pessoas.rb
@@ -1,4 +1,4 @@
-eclass AddEquipePadraoRelacionadaToConjuntoPessoas < ActiveRecord::Migration
+class AddEquipePadraoRelacionadaToConjuntoPessoas < ActiveRecord::Migration
   def change
   	add_column :conjuntos_pessoas, :equipe_padrao_relacionada, :integer, references: :conjuntos_pessoas
   end
