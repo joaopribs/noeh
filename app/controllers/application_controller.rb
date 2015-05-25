cf0859fde4fa874c4df294003e58334675381ff0
Joao Paulo Ribeiro da Silva <joaop.ribs@gmail.com>
2015-05-25 14:02:50 -0300

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -326,7 +326,7 @@ class ApplicationController < ActionController::Base
 
       # Se ainda não conseguir, salvar o problema no banco de dados
       if img_grande == "" && nome == "" && usuario_facebook == ""
-        Problema.new(problema: "Não conseguiu pegar informações de Facebook pela URL: #{url}").save
+        Problema.new(problema: "Não conseguiu pegar informações de Facebook pela URL: #{url} - ultima url de redirect: #{ultima_url}").save
       end
 
       return {
